4f29e0f3065948a6634eaa8ae1cf56e9c17e4c38
iliadimitrov1212 <iliadimitrov1212@gmail.com>
2026-07-16 20:42:49 +0000

    Add homelab monitor versions 1 and 2

---
+++ python/homelab-monitor/v2-systemd/monitor.py
@@ -0,0 +1,21 @@
+import subprocess
+machines = {
+    "u2": "192.168.56.104",
+    "r10-1": "192.168.56.101",
+    }
+def is_online(ip_address):
+	result = subprocess.run(
+		["ping", "-c", "1", "-W", "1", ip_address],
+		stdout=subprocess.DEVNULL,
+		stderr=subprocess.DEVNULL,
+	)
+
+	return result.returncode == 0
+
+
+print("=== HOMELAB STATUS ===")
+for hostname, ip_address in machines.items():
+	if is_online(ip_address):
+		print(f"{hostname} ({ip_address}) is ONLINE")
+	else:
+		print(f"{hostname} ({ip_address}) is  OFFLINE")
